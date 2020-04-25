ee70b419b9f0a8821d56d6754731188d16f6fd38
Aric Repp <aricmrepp@gmail.com>
2020-04-24 22:13:20 -0400

    Finished the project, tried adding more stuff

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,11 +1,16 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
 
+
+
 const LikeSection = props => {
+
+const [state, setState] = useState(false);
+
   return (
     <div>
       <div
@@ -13,13 +18,20 @@ const LikeSection = props => {
         key="likes-icons-container"
       >
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon onClick={() => {
+            setState(previousState => !previousState);
+            props.onChange(!state);
+          }} className={state === true ? "active" : "inactive"} 
+          icon={faHeart} />
         </div>
         <div className="like-section-wrapper">
           <FontAwesomeIcon icon={faComment} />
         </div>
+        <div className="like-time">
+          {props.timestamp}
+        </div>
       </div>
-      <p className="like-number">27 likes</p>
+        <p className="like-number">{props.likes} Likes</p>
     </div>
   )
 };
